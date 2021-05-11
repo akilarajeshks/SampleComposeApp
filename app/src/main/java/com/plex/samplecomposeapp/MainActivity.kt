3da5cf3a9e24323ec1346aa9a34883bc89f58aa6
Akila <akilarajesh20@gmail.com>
2021-05-11 23:25:26 +0100

    Added baseline lists

--- app/src/main/java/com/plex/samplecomposeapp/MainActivity.kt
+++ app/src/main/java/com/plex/samplecomposeapp/MainActivity.kt
@@ -3,11 +3,16 @@ package com.plex.samplecomposeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.plex.samplecomposeapp.ui.theme.SampleComposeAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,7 +22,7 @@ class MainActivity : ComponentActivity() {
             SampleComposeAppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    Greeting()
                 }
             }
         }
@@ -25,14 +30,52 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Greeting() {
+    val state = remember { mutableStateOf("") }
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(),
+        verticalArrangement = Arrangement.spacedBy(24.dp)
+    ) {
+        item {
+            repeat(15) {
+                LazyRow(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                ) {
+                    item {
+                        repeat(20) {
+                            Card(
+                                modifier = Modifier
+                                    .width(24.dp)
+                                    .height(24.dp)
+                            )
+                            {
+                                Text(text = "$it ")
+                            }
+                        }
+                    }
+                }
+                Button(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp),
+                    onClick = {
+                        state.value = "${state.value} hi"
+                    })
+                {
+                    Text("Button $it")
+                }
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     SampleComposeAppTheme {
-        Greeting("Android")
+        Greeting()
     }
 }
